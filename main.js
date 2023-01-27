3b4c350e4d8a55ca29994511345a528e7dd32bee
evanightly <adamhenderson3x3@gmail.com>
2023-01-27 19:38:38 +0700

    Debug

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+console.log('TOKEN:' + process.env.TOKEN)
+console.log('TOKEN 2:' + process.env['TOKEN'])
+
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
