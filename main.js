31cf5767e8c8df6efa5f3962c874a7cbacf1c799
evanightly <adamhenderson3x3@gmail.com>
2023-01-27 20:03:33 +0700

    Updated API

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const dotenv = require('dotenv')
+const express = require('express')
+
+const app = express()
+
 
 dotenv.config()
 
@@ -8,9 +12,6 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-console.log('TOKEN:' + process.env.TOKEN)
-console.log('TOKEN 2:' + process.env['TOKEN'])
-
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
@@ -19,3 +20,11 @@ client.once(Events.ClientReady, c => {
 
 // Log in to Discord with your client's token
 client.login(`${process.env['TOKEN']}`);
+
+app.get('/', (_, res) => {
+    res.send('Running')
+})
+
+app.listen(8899, () => {
+    console.log('Bot running')
+})
